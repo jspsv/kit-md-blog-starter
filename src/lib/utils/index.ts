6e2bcd536a2df69f83b6c3f9f9119868a27b470c
Joseph Sebastian <josephsv96@gmail.com>
2023-02-05 13:24:23 +0100

    fixed markdown import for each post

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,10 +1,10 @@
 import type { MarkdownContent, MarkdownFrontmatter } from '$lib/markdown/types';
 
-export const getPostsIndex = async (): Promise<MarkdownFrontmatter[]> => {
-	// import.meta.glob() does not support string literals
-	const allMarkdownModules = import.meta.glob('../../../content/*/*.md');
-	const iterableMarkdownFiles = Object.entries(allMarkdownModules);
+// import.meta.glob() does not support string literals
+const allMarkdownModules = import.meta.glob('../../../content/*/*.md');
+const iterableMarkdownFiles = Object.entries(allMarkdownModules);
 
+export const getPostsIndex = async (): Promise<MarkdownFrontmatter[]> => {
 	const allMarkdownFrontmatter = await Promise.all(
 		iterableMarkdownFiles.map(
 			async ([path, resolver]: [string, Function]): Promise<MarkdownFrontmatter> => {
@@ -23,9 +23,9 @@ export const getPostsIndex = async (): Promise<MarkdownFrontmatter[]> => {
 };
 
 export const getPostContent = async (fileSlug: string): Promise<MarkdownContent> => {
-	// const postModule = await import(`../../../content/${fileName}.md`);
-	const { Content } = await import('../../../content/dev/001-post.md');
+	const markdownResolver = allMarkdownModules[`../../../content/${fileSlug}.md`];
 
+	const { Content } = await markdownResolver();
 	const { html }: { html: MarkdownContent } = await Content();
 
 	return html;
